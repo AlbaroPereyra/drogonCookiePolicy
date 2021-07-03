57f7a735b424316ec29b47e5e62104db7635d65f
Albaro Pereyra <2AlbaroPereyra@gmail.com>
2021-07-03 14:38:45 -0700

    Added compile.sh

--- compile.sh
+++ compile.sh
@@ -1,9 +1,9 @@
 #! /bin/sh -
 
-dir=$(dirname $0);
+dir=$(pwd);
 projectName=${dir##*/};
 
 cd build/;
 cmake -DOPENSSL_ROOT_DIR=/usr/local/opt/openssl -DOPENSSL_LIBRARIES=/usr/local/opt/openssl/lib -DCMAKE_BUILD_TYPE=Release ..;
 make;
-./${projectName}
+./${projectName};
